926656e30e7c6dade545c6774404b04adf902f09
imawanmusipo <imawanmusipo@protonmail.com>
2026-04-03 22:59:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 # ==========================================
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") 
 API_URL = "https://openrouter.ai/api/v1/chat/completions"
-MODEL_AI = "qwen/qwen3-next-80b-a3b-instruct:free"
+MODEL_AI = "qwen/qwen3.6-plus:free"
 WALLET_KEY = os.getenv("WALLET_KEY") 
 
 UPDATE_BALANCE_EVERY = 5 
